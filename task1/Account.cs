05a731aaeaaf1e83c72981b6a73c23b3ca97aa2f
Pavel Bobylev <paha_ska_15@mail.ru>
2022-04-25 10:36:31 +0300

    Добавьте файлы проекта.

---
+++ task1/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Account <T>
+    {
+        T accountNumber;
+        string fullName;
+        double balance;
+       
+        public T AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+            set
+            {
+                accountNumber = value;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                fullName = value;
+            }
+        }
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public void Input()
+        {
+            Console.Write("Введите номер счёта: ");
+            AccountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.Write("Введите ФИО: ");
+            FullName = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите баланс: ");
+            Balance = Convert.ToDouble(Console.ReadLine());
+        }
+        public string GetInfo()
+        {
+            return $"\nНомер счёта: {AccountNumber} \nФИО: {FullName} \nБаланс: {Balance:F2}$";
+        }
+
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<string> account = new Account<string>();
+            account.Input();
+            Console.Write(account.GetInfo());
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.Write(account1.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
